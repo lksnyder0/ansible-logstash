143abf7e8584e095ce6eea1d10e55ea43515c561
Luke Snyder <luke@ragriz.me>
2020-02-08 00:29:29 -0500

    Moved debian scenario to custom pre_built images

---
+++ molecule/debian/tests/test_default.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+@pytest.mark.parametrize("pkg", ["logstash"])
+def test_packages(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize("svc", ["logstash"])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_config_file(host):
+    log_file = host.file("/etc/logstash/logstash.yml")
+
+    assert log_file.exists
+    assert log_file.user == "logstash"
+    assert log_file.group == "logstash"
+    assert log_file.mode == 0o0640
+
+
+def test_config_file_contents(host):
+    log_file = host.file("/etc/logstash/logstash.yml")
+
+    assert log_file.md5sum == "394bd2da7ad9c671eb21c948b8fe39b7"
